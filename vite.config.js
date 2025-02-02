a55190deb2d5f4c91565dcb280e4b94dea05b31d
Silvana Rondina <silvanarondina@MacBook-Air-de-Silvana.local>
2025-02-02 00:11:53 -0500

    Corrigindo base path para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
+  base: "/my-portfolio/", // Isso precisa estar certo!
   plugins: [react()],
 });
